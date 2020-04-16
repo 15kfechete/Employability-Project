e90b3d007f849d8341b21ed977250bdeab5ff9f0
23932872 <23932872@edgehill.ac.uk>
2020-04-16 19:28:54 +0100

    added legacy page buggy

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-import { Burger, Menu } from './components'
+import { Burger, Menu } from "./components";
 
-import Header from './components/header'
-import Footer from './components/footer'
+import Header from "./components/header";
+import Footer from "./components/footer";
 
-import LoginPage from './pages/login/login'
-import ProfilePage from './pages/profile/profile'
-import RegisterPage from './pages/register/register'
+import LoginPage from "./pages/login/login";
+import ProfilePage from "./pages/profile/profile";
+import RegisterPage from "./pages/register/register";
 
-import { AppProvider } from './AppContext'
+import { AppProvider } from "./AppContext";
 
-import './App.css'
+import "./App.css";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/admin/api',
-})
+  uri: "http://localhost:3000/admin/api",
+});
 
 function App() {
   return (
@@ -45,7 +45,7 @@ function App() {
         </Router>
       </ApolloProvider>
     </AppProvider>
-  )
+  );
 }
 
-export default App
+export default App;
